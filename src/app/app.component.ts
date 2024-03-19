759c0d68d0b3668316aa416a902c61bd00c9637f
Sebastian Lindgren <sebastian.tb.lindgren@gmail.com>
2024-03-19 12:08:56 +0200

    Simple login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { LoginDetails } from './interfaces/login-details';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'authapp';
+
+  loginDetails: LoginDetails;
+
+  constructor(auth: AuthService){
+    this.loginDetails = {
+      email:"seb@seb.seb",
+      password:"sebsebseb"
+    }
+
+    auth.loginUser(this.loginDetails);
+    
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginDetails } from '../interfaces/login-details';
+import { catchError, throwError } from 'rxjs';
+
+interface ResultData {
+  token: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl = 'http://127.0.0.1/api/';
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  constructor(private http:HttpClient) { }
+
+  loginUser(loginDetails: LoginDetails){
+    this.http.post<ResultData>(this.baseUrl+'login', loginDetails, this.httpOptions).pipe(
+      catchError(this.handleError)).subscribe(result => {
+        console.log(result);
+        localStorage.setItem("token", result.token);
+      })
+  }
+
+  private handleError(error: HttpErrorResponse){
+    if (error.status === 404) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+
+}
